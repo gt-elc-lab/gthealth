f5852416c04b54da801eefbaf6054b6fa94e4e8c
hjamal3 <hjamal@gatech.edu>
2016-02-10 12:12:53 -0500

    added some keywords

--- pipeline/classifier.py
+++ pipeline/classifier.py
@@ -10,9 +10,13 @@ class SimpleClassifier(object):
     
     def classify(self, submission):
         checker = False;
+        keyWords = ['depressed', 'depression', 'suicide', 'suicidal', 'kill', 'sad', 'unhappy',
+                    'fuck', 'fucking', 'hate', 'counseling', 'counselor', 'psychiatrist',
+                    'hate', 'death', 'die', 'heartbroken', 'lonely', 'hopeless', 'scared',
+                    'suffer', 'failure', 'therapy', 'cry', 'alone', 'loser'];
         arrayWords = submission.split();
         for word in arrayWords:
-            for x in word:
+            for x in keyWords:
                 if x in string:
                     checker = True;
         return checker;
