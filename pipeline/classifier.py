bf60b6080d034558583cd4afc5027a03c99c2136
hjamal3 <hjamal@gatech.edu>
2016-02-10 12:24:58 -0500

    efficiency fix

--- pipeline/classifier.py
+++ pipeline/classifier.py
@@ -10,14 +10,13 @@ class SimpleClassifier(object):
     
     def classify(self, submission):
         checker = False;
-        keyWords = ['depressed', 'depression', 'suicide', 'suicidal', 'kill', 'sad', 'unhappy',
+        keyWords = set(['depressed', 'depression', 'suicide', 'suicidal', 'kill', 'sad', 'unhappy',
                     'fuck', 'fucking', 'hate', 'counseling', 'counselor', 'psychiatrist',
                     'hate', 'death', 'die', 'heartbroken', 'lonely', 'hopeless', 'scared',
-                    'suffer', 'failure', 'therapy', 'cry', 'alone', 'loser'];
+                    'suffer', 'failure', 'therapy', 'cry', 'alone', 'loser']);
         arrayWords = submission.split();
         for word in arrayWords:
-            for x in keyWords:
-                if x in string:
-                    checker = True;
+            if word in keyWords:
+                checker = True;
         return checker;
             
